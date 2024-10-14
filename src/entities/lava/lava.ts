297ba840ef154f57a258104baf15cad0821007f8
Konstantin Prakhov <konstprakhov@gmail.com>
2024-10-14 16:13:26 +0200

    add vector class

--- src/entities/lava/lava.ts
+++ src/entities/lava/lava.ts
@@ -1,12 +1,27 @@
 import { AbstractCell } from '../abstractCell';
+import { CanvasHelper } from '../../shared/CanvasHelper';
 
 export class Lava extends AbstractCell {
-  constructor(x: number, y: number) {
-    super('Lava', false, x, y, 'red');
+  public width: number;
+
+  public height: number;
+
+  private canvasHelper: CanvasHelper;
+
+  constructor(x: number, y: number, width: number, height: number, canvasHelper: CanvasHelper) {
+    super('Lava', false, x, y, 'orange');
+    this.width = width;
+    this.height = height;
+    this.canvasHelper = canvasHelper;
   }
 
   interact(): void {
     // player.takeDamage(10); // Наносим урон
     // console.log('Player has DAMAGE!');
+  }
+
+  draw(): void {
+    this.canvasHelper.setFillColor(this.color);
+    this.canvasHelper.drawRectangle(this.x, this.y, this.width, this.height);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import './assets/main.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { Vector } from './entities/vector/Vector';
 
 import App from './App.vue';
 import router from './router';
 
 import { CanvasHelper } from './shared/CanvasHelper';
+import { Lava } from './entities/lava/lava';
 
 const app = createApp(App);
 
@@ -22,9 +24,7 @@ canvasHelper.setFillColor('red');
 canvasHelper.drawRectangle(10, 10, 100, 50);
 
 class Ball {
-  private vx: number;
-
-  private vy: number;
+  private velocity: Vector;
 
   private gravity: number;
 
@@ -43,13 +43,12 @@ class Ball {
     private radius: number,
     private color: string
   ) {
-    this.vx = 0;
-    this.vy = 0;
-    this.gravity = 0.5;
-    this.bounce = -0.3;
+    this.velocity = new Vector();
+    this.gravity = 0.64;
+    this.bounce = -0.4;
     this.isJumping = false;
     this.onPlatform = false;
-    this.friction = 0.95;
+    this.friction = 0.93;
   }
 
   draw() {
@@ -57,10 +56,13 @@ class Ball {
     this.canvasHelper.drawCircle(this.x, this.y, this.radius);
   }
 
-  update(platforms: Platform[]) {
-    this.vy += this.gravity;
-    this.y += this.vy;
-    this.x += this.vx;
+  update(platforms: Platform[], lavas: Lava[], deltaTime: number) {
+    const deltaSeconds = deltaTime / 20;
+
+    this.velocity.y += (this.gravity * deltaSeconds) / 1.2;
+
+    this.x += this.velocity.x * deltaSeconds;
+    this.y += this.velocity.y * deltaSeconds;
 
     this.onPlatform = false;
     platforms.forEach((platform) => {
@@ -70,74 +72,117 @@ class Ball {
         this.x + this.radius > platform.x &&
         this.x - this.radius < platform.x + platform.width
       ) {
-        if (this.vy > 0 && this.y - this.radius < platform.y) {
+        if (this.velocity.y > 0 && this.y - this.radius < platform.y) {
           this.y = platform.y - this.radius;
-          this.vy = 0;
+          this.velocity.y = 0;
           this.isJumping = false;
           this.onPlatform = true;
-          this.vx = 0;
-        } else if (this.vy < 0 && this.y + this.radius > platform.y + platform.height) {
+          this.velocity.x *= this.friction;
+
+          if (Math.abs(this.velocity.x) < 0.001) {
+            this.velocity.x = 0;
+          }
+        } else if (this.velocity.y < 0 && this.y + this.radius > platform.y + platform.height) {
           this.y = platform.y + platform.height + this.radius;
-          this.vy = 0;
+          this.velocity.y = 0;
         }
       }
     });
 
     if (this.y + this.radius > this.canvasHelper.getHeight()) {
       this.y = this.canvasHelper.getHeight() - this.radius;
-      this.vy *= this.bounce;
+      this.velocity.y *= this.bounce;
       this.isJumping = false;
       this.onPlatform = true;
     }
 
     if (this.x + this.radius > this.canvasHelper.getWidth() || this.x - this.radius < 0) {
-      this.vx = -this.vx;
+      this.velocity.x = -this.velocity.x;
     }
 
     if (!this.isJumping && this.onPlatform) {
-      this.vx *= this.friction;
+      this.velocity.x *= this.friction;
 
-      if (Math.abs(this.vx) < 0.1) {
-        this.vx = 0;
+      if (Math.abs(this.velocity.x) < 0.5) {
+        this.velocity.x = 0;
+      }
+    }
+
+    lavas.forEach((lava) => {
+      if (
+        this.y + this.radius > lava.getPosition().y &&
+        this.y - this.radius < lava.getPosition().y + lava.height &&
+        this.x + this.radius > lava.getPosition().x &&
+        this.x - this.radius < lava.getPosition().x + lava.width
+      ) {
+        this.resetPosition();
+      }
+    });
+
+    if (this.y + this.radius > this.canvasHelper.getHeight()) {
+      this.y = this.canvasHelper.getHeight() - this.radius;
+      this.velocity.y *= this.bounce;
+      this.isJumping = false;
+      this.onPlatform = true;
+    }
+
+    if (this.x + this.radius > this.canvasHelper.getWidth() || this.x - this.radius < 0) {
+      this.velocity.x = -this.velocity.x;
+    }
+
+    if (!this.isJumping && this.onPlatform) {
+      this.velocity.x *= this.friction;
+
+      if (Math.abs(this.velocity.x) < 0.5) {
+        this.velocity.x = 0;
       }
     }
   }
 
   jump() {
     if (this.onPlatform) {
-      this.vy = -10;
+      this.velocity.y = -10;
       this.isJumping = true;
     }
   }
 
   moveLeft() {
     if (!this.isJumping) {
-      this.vx = -5;
+      this.velocity.x = -5;
     }
   }
 
   moveRight() {
     if (!this.isJumping) {
-      this.vx = 5;
+      this.velocity.x = 5;
     }
   }
 
   stop() {
     if (!this.isJumping) {
-      this.vx = 0;
+      this.velocity.x = 0;
     }
   }
 
   setVelocityX(value: number) {
-    this.vx = value;
+    this.velocity.x = value;
   }
 
   getVelocityX(): number {
-    return this.vx;
+    return this.velocity.x;
   }
 
   getIsJumping(): boolean {
     return this.isJumping;
+  }
+
+  private resetPosition() {
+    this.x = 100;
+    this.y = this.canvasHelper.getHeight() - 25;
+    this.velocity.x = 0;
+    this.velocity.y = 0;
+    this.isJumping = false;
+    this.onPlatform = false;
   }
 }
 
@@ -162,14 +207,39 @@ const platforms = [
   new Platform(canvasHelper, 150, 500, 100, 10),
   new Platform(canvasHelper, 300, 400, 100, 10),
   new Platform(canvasHelper, 450, 300, 100, 10),
-  new Platform(canvasHelper, 600, 200, 100, 10)
+  new Platform(canvasHelper, 600, 200, 100, 10),
+  new Platform(canvasHelper, 0, canvasHelper.getHeight() - 10, canvasHelper.getWidth(), 10),
+  new Platform(canvasHelper, 0, 0, canvasHelper.getWidth(), 10),
+  new Platform(canvasHelper, 0, 0, 10, canvasHelper.getHeight()),
+  new Platform(canvasHelper, canvasHelper.getWidth() - 10, 0, 10, canvasHelper.getHeight())
 ];
 
-function animate() {
+const lavas = [
+  new Lava(250, 480, 50, 20, canvasHelper),
+  new Lava(400, 380, 50, 20, canvasHelper),
+  new Lava(550, 280, 50, 20, canvasHelper),
+  new Lava(700, 180, 50, 20, canvasHelper)
+];
+
+let lastTime = 0;
+
+function animate(time: number) {
+  if (lastTime === 0) {
+    lastTime = time;
+    requestAnimationFrame(animate);
+    return;
+  }
+
+  const deltaTime = time - lastTime;
+
+  lastTime = time;
   canvasHelper.clearCanvas();
-  ball.update(platforms);
+  ball.update(platforms, lavas, deltaTime);
   ball.draw();
   platforms.forEach((platform) => platform.draw());
+
+  lavas.forEach((lava) => lava.draw());
+
   requestAnimationFrame(animate);
 }
 
@@ -199,4 +269,4 @@ document.addEventListener('keyup', function (e) {
   }
 });
 
-animate();
+animate(performance.now());
