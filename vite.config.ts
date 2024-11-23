a990309375a2bbd5313bf349672aceff76839a71
Dmitriy Arkhipov <dmitriy.s.arkhipov@gmail.com>
2024-11-23 16:28:13 +0300

    Resolved prod building problems, added new level

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { fileURLToPath, URL } from 'node:url';
+import { loadEnv } from 'vite';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/jumping-journey/'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV)
+    },
+    base: mode === 'production' ? '' : '/jumping-journey' // For GitHub Pages
+  };
 });
